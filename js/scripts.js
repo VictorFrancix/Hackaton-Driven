0499c0edb46a86e82c1dcaff4543b59c9c1da13f
antcarlosrbj <antoniocarlosrbj@gmail.com>
2022-03-04 20:47:37 -0300

    Temperatura na tela

--- js/scripts.js
+++ js/scripts.js
@@ -72,49 +72,8 @@ function showTemp() {
         "name": "Parangaba",
         "cod": 200
     };
+
+    
+    let clima = document.querySelector(".resposta");
+    clima.innerHTML = tempServidor.main.temp;
 }
-
-tempServidor = {
-    "coord": {
-        "lon": -38.5778,
-        "lat": -3.7716
-    },
-    "weather": [
-        {
-            "id": 801,
-            "main": "Clouds",
-            "description": "few clouds",
-            "icon": "02n"
-        }
-    ],
-    "base": "stations",
-    "main": {
-        "temp": 27.93,
-        "feels_like": 31.63,
-        "temp_min": 27.93,
-        "temp_max": 28.31,
-        "pressure": 1010,
-        "humidity": 78
-    },
-    "visibility": 10000,
-    "wind": {
-        "speed": 2.57,
-        "deg": 60
-    },
-    "clouds": {
-        "all": 20
-    },
-    "dt": 1646436175,
-    "sys": {
-        "type": 1,
-        "id": 8363,
-        "country": "BR",
-        "sunrise": 1646383269,
-        "sunset": 1646427075
-    },
-    "timezone": -10800,
-    "id": 3393046,
-    "name": "Parangaba",
-    "cod": 200
-};
-
