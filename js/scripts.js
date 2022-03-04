9c4e7d4d4633c0b62aca39d076ed3f32c7bf3a19
João Victor Francisco <Joo Victor>
2022-03-04 20:22:54 -0300

    atualização

--- js/scripts.js
+++ js/scripts.js
@@ -14,3 +14,7 @@ function accessDenied(erro) {
 }
 
 getLocation();
+
+function getTemperature(){
+    const promise = axios.get(`https://api.openweathermap.org/data/2.5/weather?lat={}&lon={}&appid=bf7f435975e801f6a8b91919e3d6a5ab&units=metric`)
+}
